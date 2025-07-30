a330718add4de8f38794bd200784a05c61c601c2
syyy-99 <eunseoyeong79@gmail.com>
2025-07-31 01:22:17 +0900

    파일 이름 변경

--- app/src/main/java/com/example/projectmate/LoginActivity.kt
+++ app/src/main/java/com/example/projectmate/LoginActivity.kt
@@ -5,10 +5,7 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
@@ -23,7 +20,7 @@ class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_login1)
 
         auth = Firebase.auth
 
--- app/src/main/java/com/example/projectmate/MainActivity.kt
+++ app/src/main/java/com/example/projectmate/MainActivity.kt
@@ -3,10 +3,7 @@ package com.example.projectmate
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
     private lateinit var loginButton: Button
@@ -14,7 +11,7 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main1)
 
         loginButton = findViewById(R.id.LoginButton)
         teamLoginButton = findViewById(R.id.TeamLoginButton)
